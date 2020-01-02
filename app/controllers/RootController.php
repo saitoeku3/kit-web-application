a6cad29d6d8f137c752e3ec0d6f504325e38aeb3
Tadao Iseki <saitoeku3@gmail.com>
2020-01-03 00:27:12 +0900

    Add header

--- app/controllers/RootController.php
+++ app/controllers/RootController.php
@@ -1,9 +1,9 @@
 <?
-require dirname(__FILE__) . "/../views/_template.php";
+require __DIR__ . "/../views/layouts/template.php";
 
 class RootController {
   public function index() {
-    $title = "index";
+    $title = "トップ";
     $body = file_get_contents(__DIR__ . '/../views/index.php');
     echo template($title, $body);
   }
---
+++ app/views/layouts/header.php
@@ -0,0 +1,25 @@
+<nav class="navbar navbar-dark bg-dark" style="display: flex; justify-content: space-between;">
+  <a class="navbar-brand" href="/kit-web-application/" style="display: block;">KIT Web Application</a>
+  <form style="display: flex; width: 50%;">
+    <input class="form-control mr-sm-2" type="search" placeholder="キーワードを入力" aria-label="Search">
+    <input class="btn btn-outline-success" type="submit" value="検索" style="width: 100px" />
+  </form>
+  <ul class="nav">
+    <li class="nav-item">
+      <a class="nav-link" href="#" style="color: #fff;">
+        新規登録
+      </a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="#" style="color: #fff;">
+        ログイン
+      </a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="#" style="color: #fff;">
+        カート
+        <i class="fas fa-shopping-cart"></i>
+      </a>
+    </li>
+  </ul>
+</nav>
