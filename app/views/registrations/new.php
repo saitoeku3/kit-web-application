387c941c0d52bb698e5d3ebf28805f051962fe4d
Tadao Iseki <saitoeku3@gmail.com>
2020-01-04 01:36:32 +0900

    Fix view function

--- app/views/registrations/new.php
+++ app/views/registrations/new.php
@@ -1,36 +1,33 @@
-<h2 style="margin-bottom: 32px;">新規登録</h2>
-<form action="/kit-web-application/signup" method="post">
-  <div class="form-group">
-    <label style="margin: auto 0;">氏名</label>
-    <div style="display: flex; justify-content: space-between; width: 100%;">
-      <input type="text" name="lastname" class="form-control" style="width: 48%;" placeholder="姓">
-      <input type="text" name="firstname" class="form-control" style="width: 48%;" placeholder="名">
+<div class="container" style="margin-top: 32px;">
+  <h2 style="margin-bottom: 32px;">新規登録</h2>
+  <form action="/kit-web-application/sign-up" method="post">
+    <div class="form-group">
+      <label style="margin: auto 0;">氏名</label>
+      <div style="display: flex; justify-content: space-between; width: 100%;">
+        <input type="text" name="lastname" class="form-control" style="width: 48%;" placeholder="姓">
+        <input type="text" name="firstname" class="form-control" style="width: 48%;" placeholder="名">
+      </div>
     </div>
-  </div>
-  <div class="form-group">
-    <label style="margin: auto 0;">メールアドレス</label>
-    <input type="text" name="email" class="form-control">
-  </div>
-  <div class="form-group">
-    <label style="margin: auto 0;">パスワード</label>
-    <input type="password" name="password" class="form-control">
-  </div>
-  <div class="form-group">
-  <label style="margin: auto 0;">パスワード (再入力)</label>
-    <input type="password" name="password_re" class="form-control">
-  </div>
-  <div class="form-group">
-    <label style="margin: auto 0;">郵便番号</label>
-    <input type="text" name="zip_code" class="form-control">
-  </div>
-  <div class="form-group">
-    <label style="margin: auto 0;">住所</label>
-    <input type="text" name="address" class="form-control">
-  </div>
-  <button type="submit" class="btn btn-primary">登録</button>
-</form>
-
-<div><?
-session_start();
-$_SESSION['name']->name
-?></div>
+    <div class="form-group">
+      <label style="margin: auto 0;">メールアドレス</label>
+      <input type="text" name="email" class="form-control">
+    </div>
+    <div class="form-group">
+      <label style="margin: auto 0;">パスワード</label>
+      <input type="password" name="password" class="form-control">
+    </div>
+    <div class="form-group">
+    <label style="margin: auto 0;">パスワード (再入力)</label>
+      <input type="password" name="password_re" class="form-control">
+    </div>
+    <div class="form-group">
+      <label style="margin: auto 0;">郵便番号</label>
+      <input type="text" name="zip_code" class="form-control">
+    </div>
+    <div class="form-group">
+      <label style="margin: auto 0;">住所</label>
+      <input type="text" name="address" class="form-control">
+    </div>
+    <button type="submit" class="btn btn-primary">登録</button>
+  </form>
+</div>
--- index.php
+++ index.php
@@ -4,17 +4,16 @@ require __DIR__ . '/./app/controllers/CartsController.php';
 require __DIR__ . '/./app/controllers/RootController.php';
 require __DIR__ . '/./app/controllers/RegistrationsController.php';
 require __DIR__ . '/./app/controllers/SessionsController.php';
-require __DIR__ . "/./app/views/layouts/template.php";
 
 $router = new AltoRouter();
 $router->setBasePath('/kit-web-application');
-$router->map('GET',    '/',       'RootController#index');
-$router->map('GET',    '/sign-up', 'RegistrationsController#new');
-$router->map('POST',   '/sign-up', 'RegistrationsController#create');
-$router->map('GET',    '/sign-in', 'SessionsController#new');
-$router->map('POST',   '/sign-in', 'SessionsController#create');
+$router->map('GET',    '/',         'RootController#index');
+$router->map('GET',    '/sign-up',  'RegistrationsController#new');
+$router->map('POST',   '/sign-up',  'RegistrationsController#create');
+$router->map('GET',    '/sign-in',  'SessionsController#new');
+$router->map('POST',   '/sign-in',  'SessionsController#create');
 $router->map('DELETE', '/sign-out', 'SessionsController#destroy');
-$router->map('GET',    '/carts',  'CartsController#index');
+$router->map('GET',    '/carts',    'CartsController#index');
 $match = $router->match();
 
 list( $controller, $action ) = explode( '#', $match['target'] );
