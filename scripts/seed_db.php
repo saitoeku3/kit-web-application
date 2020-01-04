b4f903df0ac5e11eaf1d4022c6200b9122ed2dfd
Tadao Iseki <saitoeku3@gmail.com>
2020-01-05 06:33:16 +0900

    Add migration script

---
+++ scripts/seed_db.php
@@ -0,0 +1,11 @@
+<?
+require __DIR__ . '/../db/connect.php';
+require __DIR__ . '/../app/models/Product.php';
+
+$products = json_decode(file_get_contents(__DIR__ . '/../db/seeds/products.json'), true)['data'];
+
+foreach ($products as $params) {
+  $product = new Product($params);
+  $product.save();
+}
+?>
