74b1f5f1d651c55e9ec86189d47a068a304d759e
Tadao Iseki <saitoeku3@gmail.com>
2020-01-05 20:01:36 +0900

    Add order_history delete action

--- app/controllers/CartsController.php
+++ app/controllers/CartsController.php
@@ -1,10 +1,33 @@
 <?
-require_once dirname(__FILE__) . "/./ApplicationController.php";
+require_once __DIR__ . "/./ApplicationController.php";
+require_once __DIR__ . "/../models/OrderHistory.php";
 
 class CartsController extends ApplicationController {
   public function index() {
+    $products = array();
+
+    if(!isset($_SESSION)){
+      session_start();
+    }
+
+    if (isset($_SESSION['id'])) {
+      $products = OrderHistory::find_products_by_user_id($_SESSION['id']);
+    }
+
     $title = "カート";
     $body = __DIR__ . '/../views/carts/index.php';
-    echo view($title, $body);
+    $data = array('products' => $products);
+    echo view($title, $body, $data);
+  }
+
+  public function destroy() {
+    $reg_result;
+    $queries;
+
+    preg_match('/\d$/', $_SERVER["REQUEST_URI"], $reg_result);
+    $id = $reg_result[0];
+    OrderHistory::destroy($id);
+    header('Location: http://'.$_SERVER['SERVER_NAME'].'/kit-web-application/carts', true, 302);
+    exit();
   }
 }
--- index.php
+++ index.php
@@ -1,24 +1,26 @@
 <?
 require __DIR__ . '/./vendor/autoload.php';
-require __DIR__ . '/./app/controllers/ProductsController.php';
 require __DIR__ . '/./app/controllers/CartsController.php';
-require __DIR__ . '/./app/controllers/RootController.php';
+require __DIR__ . '/./app/controllers/ProductsController.php';
 require __DIR__ . '/./app/controllers/RegistrationsController.php';
-require __DIR__ . '/./app/controllers/SessionsController.php';
+require __DIR__ . '/./app/controllers/RootController.php';
 require __DIR__ . '/./app/controllers/SearchController.php';
+require __DIR__ . '/./app/controllers/SessionsController.php';
 
 $router = new AltoRouter();
 $router->setBasePath('/kit-web-application');
 
-$router->map('GET',  '/',         'RootController#index');
-$router->map('GET',  '/sign-up',  'RegistrationsController#new');
-$router->map('POST', '/sign-up',  'RegistrationsController#create');
-$router->map('GET',  '/sign-in',  'SessionsController#new');
-$router->map('POST', '/sign-in',  'SessionsController#create');
-$router->map('POST', '/sign-out', 'SessionsController#destroy'); // DELETE
-$router->map('GET',  '/carts',    'CartsController#index');
-$router->map('GET',  '/products/[i:id]','ProductsController#show');
-$router->map('GET',  '/search',   'SearchController#index');
+$router->map('GET',  '/',                'RootController#index');
+$router->map('GET',  '/carts',           'CartsController#index');
+$router->map('POST', '/carts/[i:id]',    'CartsController#destroy'); // DELETE
+$router->map('GET',  '/products/[i:id]', 'ProductsController#show');
+$router->map('GET',  '/search',          'SearchController#index');
+$router->map('GET',  '/sign-in',         'SessionsController#new');
+$router->map('POST', '/sign-in',         'SessionsController#create');
+$router->map('GET',  '/sign-up',         'RegistrationsController#new');
+$router->map('POST', '/sign-up',         'RegistrationsController#create');
+$router->map('POST', '/sign-out',        'SessionsController#destroy'); // DELETE
+
 $match = $router->match();
 
 list( $controller, $action ) = explode( '#', $match['target'] );
