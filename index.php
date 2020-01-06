981777814057a5e7536b8e7cf69c551a5f9a847c
daichi77 <b1738164@planet.kanazawa-it.ac.jp>
2020-01-07 05:43:00 +0900

    カート画面で商品の個数を変えれるように変更

--- index.php
+++ index.php
@@ -6,6 +6,7 @@ require __DIR__ . '/./app/controllers/RegistrationsController.php';
 require __DIR__ . '/./app/controllers/RootController.php';
 require __DIR__ . '/./app/controllers/SearchController.php';
 require __DIR__ . '/./app/controllers/SessionsController.php';
+require __DIR__ . '/./app/controllers/OrdersController.php';
 
 $router = new AltoRouter();
 $router->setBasePath('/kit-web-application');
@@ -14,6 +15,10 @@ $router->map('GET',  '/',                'RootController#index');
 $router->map('GET',  '/carts',           'CartsController#index');
 $router->map('POST', '/carts',           'CartsController#create');
 $router->map('POST', '/carts/[i:id]',    'CartsController#destroy'); // DELETE
+$router->map('GET',  '/orders',          'OrdersController#index');
+$router->map('GET',  '/orders/new',      'OrdersController#new');
+$router->map('POST', '/orders',          'OrdersController#create');
+$router->map('POST',  '/orders/edit',    'OrdersController#edit'); //PATCHにしたい
 $router->map('GET',  '/products/[i:id]', 'ProductsController#show');
 $router->map('GET',  '/search',          'SearchController#index');
 $router->map('GET',  '/sign-in',         'SessionsController#new');
