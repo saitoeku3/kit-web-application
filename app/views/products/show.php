2158e2c5ba5f5ae087ff880dcf1f0c2b7d3fc394
daichi77 <b1738164@planet.kanazawa-it.ac.jp>
2020-01-08 16:24:18 +0900

    productページにカテゴリーリストを追加

--- app/views/products/show.php
+++ app/views/products/show.php
@@ -1,19 +1,18 @@
 <div class="wrapper">
     <div class="list-group" id="junre-box">
         <div class="list-group-item" style="background-color: #f5f5f5">ジャンル一覧</div>
-        <a href="#" class="list-group-item">スポーツ</a>
-        <a href="#" class="list-group-item">食べ物</a>
-        <a href="#" class="list-group-item">漫画</a>
-        <a href="#" class="list-group-item">旅行</a>
+        <? foreach ($categorys as $category) { ?>
+          <a href="#" class="list-group-item"><? echo $category ?></a>
+        <? }?>
     </div>
     <img src="" class="product-img">
     <div class="card">
         <div class="card-body">
-            <h4 class="card-title"><?= $name ?></h4>
-            <p class="card-text">【価格】<?= $price ?>円</p>
-            <p class="card-text">【商品説明】<br><?= $description ?></p>
+            <h4 class="card-title"><?= $product['name'] ?></h4>
+            <p class="card-text">【価格】<?= $product['price'] ?>円</p>
+            <p class="card-text">【商品説明】<br><?= $product['description'] ?></p>
             <form method="post" action="http://192.168.64.2/kit-web-application/carts">
-                <input type="hidden" value=<?= $id ?> name="product_id">
+                <input type="hidden" value=<?= $product['id'] ?> name="product_id">
                 <button type="submit" class="btn btn-primary">購入する</button>
             </form>
         </div>
